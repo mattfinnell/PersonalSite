d89c63bf9a60102c86a632a603f6d93e76bbef91
Matt Finnell <mattfinnell104@gmail.com>
2017-07-03 18:48:54 -0700

    configured google analytics only for prod. Implemented template heiarchy

--- site/config.py
+++ site/config.py
@@ -4,9 +4,10 @@ basedir  = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object) :
     APP_NAME = "__mattfinnell_dot_io_website__"
+    GOOGLE_ANALYTICS = False
     CSRF_ENABLED = True
     DEVELOPMENT = False
-    Testing = False
+    TESTING = False
     DEBUG = True
 
 class DevelopmentConfig(Config) :
@@ -19,4 +20,5 @@ class StagingConfig(Config) :
     DEVELOPMENT = True
 
 class ProductionConfig(Config) :
+    GOOGLE_ANALYTICS = True
     DEBUG = False
