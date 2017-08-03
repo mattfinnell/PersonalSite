046fc7e8e703398af817ed8e5fdb184496a56f5b
Matt Finnell <mattfinnell104@gmail.com>
2017-08-02 21:32:50 -0700

    [Nasty Commit] restructuring all of the SASS

--- site/app.py
+++ site/app.py
@@ -1,18 +1,45 @@
+# Flask Framework
 from flask import Flask, render_template
+
+# Utilities
 from datetime import datetime
-
-from pprint import pprint
-
 import config
 import utils
 import os
 
+# Initialize application and application properties
 app = Flask(config.Config.APP_NAME)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def index() :
+    utils.compile_sass(app.config)
+    return render_template("index.html")
+
+@app.route("/landing")
+def landing() :
+    utils.compile_sass(app.config)
     return render_template("landing.html")
+
+@app.route("/about/")
+def about() :
+    utils.compile_sass(app.config)
+    return render_template("about.html")
+
+@app.route("/test_elements")
+def elements() :
+    utils.compile_sass(app.config)
+    return render_template("elements.html")
+
+@app.route("/test_generic")
+def generic() :
+    utils.compile_sass(app.config)
+    return render_template("generic.html")
+
+@app.route("/test")
+def test_index() :
+    utils.compile_sass(app.config)
+    return render_template("test.html")
 
 @app.context_processor
 def jinja_addons() :
--- site/utils.py
+++ site/utils.py
@@ -4,15 +4,12 @@ import sass
 import os
 
 def _clear_directory(dir_path) :
-    for f in os.listdir(dir_path) :
-        try :
-            shutil.rmtree(f)
-        except :
-            pass # Not the best practice but it serves it's purpose perfectly
+    if os.path.isdir(os.getcwd() + dir_path) :
+            shutil.rmtree(os.getcwd() + dir_path)
 
 def compile_sass(env) :
 
-    _clear_directory("static/css")
+    _clear_directory("/static/css")
 
     sass.compile(
         dirname = (env["SASS_DIR"], env["CSS_DIR"]),
