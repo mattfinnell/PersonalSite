997a729efd65fef036a4b6d155072fe41bd5135c
Matt Finnell <mattfinnell104@gmail.com>
2017-08-18 16:54:27 -0700

    gunicorn can safely run the app, meaning that heroku can now safely run the app

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,7 @@
 # Run site
 cd site
+
 npm install postcss-cli
 npm install autoprefixer
-gunicorn app:app --log-file=-
+
+gunicorn run:app
--- run.sh
+++
@@ -1,5 +0,0 @@
-cd site
-export FLASK_APP=run.py
-export FLASK_DEBUG=true
-
-flask run
