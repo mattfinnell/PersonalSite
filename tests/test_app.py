a40fc913a66e31d3edebaed8d9b9b4ddb6520dfd
Matt Finnell <mattfinnell104@gmail.com>
2017-08-23 19:55:24 -0700

    Tinkering with pytest and removed useless images

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,21 @@
+# Frameworks
+from flask import Flask
+import pytest
+
+# Application modules
+from website.app import create_app
+import website.config as config
+import website.utils as utils
+
+@pytest.fixture
+def blank_app() :
+    return Flask(__name__)
 
 def test_create_app() :
-    from website.app import create_app
-    import website.config as config
-
     app = create_app(config.DevelopmentConfig)
 
+    assert app is not None
+
+def test_blank_app() :
+    app = blank_app()
     assert app is not None
