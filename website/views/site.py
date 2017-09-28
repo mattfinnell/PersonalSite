b7b5700645c8447449d24c4abc9faa8ed51f0071
Matt Finnell <mattfinnell104@gmail.com>
2017-09-27 18:14:05 -0700

    Removed endpoints for pages that are still in development

--- website/views/site.py
+++ website/views/site.py
@@ -6,17 +6,8 @@ from website.models import Skill
 site = Blueprint('site', __name__, template_folder="../templates")
 
 @site.route("/")
-@site.route("/about")
-@site.route("/about/")
 def about() :
     skills = list(reversed(list(Skill.query.order_by(Skill.percent))))
     data = {"skills" : skills}
+
     return render("about.html", data=data)
-
-@site.route("/card")
-def card() :
-    return render("card.html")
-
-@site.route("/landing")
-def landing() :
-    return render("landing.html")
