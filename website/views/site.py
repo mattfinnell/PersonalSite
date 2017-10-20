ee7ae9d74f15eeb4074b4a8c6588f87229b929cb
Matt Finnell <mattfinnell104@gmail.com>
2017-10-19 21:48:24 -0700

    lazy redirect, mapped /about to /

--- website/views/site.py
+++ website/views/site.py
@@ -1,15 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 from website.models import Skill
 
 site = Blueprint('site', __name__, template_folder="../templates")
 
 @site.route("/")
-def about() :
+def index() :
 
     skills = list(reversed(list(Skill.query.order_by(Skill.percent))))
     data = {"skills" : skills}
     return render_template("about.html", data=data)
+
+@site.route("/about")
+def about() :
+    return redirect('/')
 
 @site.route("/card")
 def card() :
