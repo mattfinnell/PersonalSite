1fdb8281f723fb3e3e1044bc3b9f4e7b36042486
Matt Finnell <mattfinnell104@gmail.com>
2017-10-22 01:28:27 -0700

    docstringed the one and only util function

--- website/utils.py
+++ website/utils.py
@@ -1,5 +1,8 @@
-
 def get_classes_of_type(module, class_type):
+    """ Within a module, there can be many class definitions. This filters all
+    classes within a module that inherit from class_type. Useful for getting
+    all blueprints from a views module, or orm objects from a models module.
+    """
     object_names = filter(
         lambda x : issubclass(type(getattr(module, x)), class_type),
         module.__dict__
