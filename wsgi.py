ab991ffdf453bf2bbc32e8e94cb65fb76aa4d9bc
Matt Finnell <mattfinnell104@gmail.com>
2018-03-23 00:00:59 -0700

    Milliman Update

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 from website.app import create_app
 import website.config as config
-
 import os
 
 config_object = eval(os.environ['FLASK_APP_CONFIG'])
 app = create_app(config_object)
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     app.run()
